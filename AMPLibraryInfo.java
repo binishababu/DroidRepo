6a521eaab7861d6dca06cf82557bfcddb2019984
Binisha Babu <bbabu@akamai.com>
2017-05-25 16:30:56 +0530

    Updating Release VERSION and SPRINT for release build

--- AMPLibraryInfo.java
+++ AMPLibraryInfo.java
@@ -4,7 +4,7 @@ public class AMPLibraryInfo {
 
     public static final String NAME = "Akamai's Android AMP SDK";
 
-    public static final String VERSION = "$RELEASE_VERSION"
+    public static final String VERSION = ${RELEASE_VERSION}
 
     public static boolean isUplynk = false;
 
