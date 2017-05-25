8a9ace32f7081b4ce6883fb45278cc9addca1177
Binisha Babu <bbabu@akamai.com>
2017-05-25 16:18:23 +0530

    Updating Release VERSION and SPRINT for release build

---
+++ AMPLibraryInfo.java
@@ -0,0 +1,11 @@
+package com.akamai.media.elements;
+
+public class AMPLibraryInfo {
+
+    public static final String NAME = "Akamai's Android AMP SDK";
+
+    public static final String VERSION = "$RELEASE_VERSION"
+
+    public static boolean isUplynk = false;
+
+}
---
+++ project.sh
@@ -0,0 +1,9 @@
+echo 'Exporting Project Constants'
+
+export SPRINT=
+export MAJOR_VERSION=6
+export SVN_REV=0
+export RELEASE_VERSION="$MAJOR_VERSION.$SPRINT"
+export BASE_DIR="/jenkins-workspace"
+export BASE_DIR="$HOME/Documents/Projects"
+export RELEASE_FULLNAME="AndroidAMP-$RELEASE_NAME-$RELEASE_VERSION"
